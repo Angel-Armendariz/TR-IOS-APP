722f014a46b568a496b8d9656689b9471c20cd91
Angel-Armendariz <angela126509@gmail.com>
2023-10-16 19:00:28 -0700

    Inital Commit

---
+++ IOS Webview App/IOS_Webview_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Webview_AppApp.swift
+//  IOS Webview App
+//
+//  Created by Titan Comm on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Webview_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
